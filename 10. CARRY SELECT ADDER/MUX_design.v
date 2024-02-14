9e26c80db5214a96de29e381868563f98c659b24
Sreeprad <110930203+Sreeprad@users.noreply.github.com>
2024-02-14 23:05:57 +0530

    Add files via upload

---
+++ 10. CARRY SELECT ADDER/MUX_design.v
@@ -0,0 +1,12 @@
+module MUX_design (
+    input D0,
+    input D1,
+    input S,
+    output Vout
+    );
+    wire w1, w2, w3;
+    not U1(w1, S);
+    and U2(w2, D0, w1);
+    and U3(w3, D1, S);
+    or U4(Vout, w2, w3);
+endmodule
