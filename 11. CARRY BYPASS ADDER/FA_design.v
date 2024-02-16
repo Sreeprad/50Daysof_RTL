bbbe05b3b03176613a5279999dda22f52930c32c
Sreeprad <110930203+Sreeprad@users.noreply.github.com>
2024-02-16 06:37:50 +0530

    Add files via upload

---
+++ 11. CARRY BYPASS ADDER/FA_design.v
@@ -0,0 +1,19 @@
+module FA_design(
+    input A,
+    input B,
+    input C,
+    output Sum,
+    output Carry
+    );
+
+    wire X1, X2, X3;
+    wire C1, C2;
+
+    xor (X1, A, B);
+    xor (Sum, X1, C);
+
+    and (C1, A, B);
+    and (C2, C, X1);
+    or (Carry, C1, C2);
+
+endmodule
