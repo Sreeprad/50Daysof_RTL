0ad91ba8d48745a9a8c95f81f722322bd43c470c
Sreeprad <110930203+Sreeprad@users.noreply.github.com>
2024-02-17 12:53:46 +0530

    Update Comparator_TB.v

--- 7. COMPARATOR/2_BIT_COMPRATOR/Comparator_TB.v
+++ 7. COMPARATOR/2_BIT_COMPRATOR/Comparator_TB.v
@@ -1,16 +1,18 @@
 module Comparator_TB;
 
-reg A, B;
+reg [1:0] A, B;
 wire A_grt_B, A_less_B, A_eq_B;
 
 Comparator_design uut(A, B, A_grt_B, A_less_B, A_eq_B);
 
 initial begin
-    A = 1'b0; B = 1'b0; #10;
-    A = 1'b0; B = 1'b1; #10;
-    A = 1'b1; B = 1'b0; #10;
-    A = 1'b1; B = 1'b1; #10;
+    A = 2'b00; B = 2'b11; #10;
+    A = 2'b01; B = 2'b10; #10;
+    A = 2'b11; B = 2'b11; #10;
+    A = 2'b10; B = 2'b00; #10;
+    A = 2'b01; B = 2'b10; #10;
     $finish();
 end
 
 endmodule
+
