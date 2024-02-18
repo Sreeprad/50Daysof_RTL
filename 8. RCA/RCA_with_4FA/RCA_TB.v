1d100ac288c4cf654af137bc3234ba8338b34bc2
Sreeprad <mandasreeprad1104@gmail.com>
2024-02-18 10:24:44 +0530

    chnages added

--- 8. RCA/RCA_with_4FA/RCA_TB.v
+++ 8. RCA/RCA_with_4FA/RCA_TB.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module RCA_TB;
 
     reg A0, A1, A2, A3, B0, B1, B2, B3, Cin = 0;
@@ -36,42 +35,3 @@ module RCA_TB;
     end
 
 endmodule
-=======
-module RCA_TB;
-
-    reg A0, A1, A2, A3, B0, B1, B2, B3, Cin = 0;
-    wire Cout, S0, S1, S2, S3;
-
-    RCA_design uut(
-        .A0(A0),
-        .A1(A1),
-        .A2(A2),
-        .A3(A3),
-        .B0(B0),
-        .B1(B1),
-        .B2(B2),
-        .B3(B3),
-        .Cin(Cin),
-        .Cout(Cout),
-        .S0(S0),
-        .S1(S1),
-        .S2(S2),
-        .S3(S3)
-    );
-
-    initial begin
-        A0 = 0; B0 = 0; A1 = 1; B1 = 0; A2 = 0; B2 = 0; A3 = 1; B3 = 0;
-        #20
-        A0 = 1; B0 = 0; A1 = 1; B1 = 1; A2 = 0; B2 = 1; A3 = 1; B3 = 1;
-        #20
-        A0 = 1; B0 = 1; A1 = 1; B1 = 1; A2 = 1; B2 = 1; A3 = 1; B3 = 1;
-        #20
-        A0 = 0; B0 = 1; A1 = 1; B1 = 1; A2 = 1; B2 = 0; A3 = 1; B3 = 1;
-        #20
-        A0 = 1; B0 = 0; A1 = 1; B1 = 0; A2 = 1; B2 = 1; A3 = 1; B3 = 1;
-        #20
-        $finish();
-    end
-
-endmodule
->>>>>>> 3e3c4830248ac0eb2b91db77ea6c2fab36e4a912
--- 8. RCA/RCA_with_4FA/RCA_design.v
+++ 8. RCA/RCA_with_4FA/RCA_design.v
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 module RCA_design(
     input A0,
     input A1,
@@ -24,30 +22,3 @@ module RCA_design(
     FA_design FA4(A3, B3, C3, S3, Cout);
 
 endmodule
-=======
-
-module RCA_design(
-    input A0,
-    input A1,
-    input A2,
-    input A3,
-    input B0,
-    input B1,
-    input B2,
-    input B3,
-    input Cin,
-    output Cout,
-    output S0,
-    output S1,
-    output S2,
-    output S3
-);
-
-    wire C1, C2, C3;
-    FA_design FA1(A0, B0, Cin, S0, C1);
-    FA_design FA2(A1, B1, C1, S1, C2);
-    FA_design FA3(A2, B2, C2, S2, C3);
-    FA_design FA4(A3, B3, C3, S3, Cout);
-
-endmodule
->>>>>>> 3e3c4830248ac0eb2b91db77ea6c2fab36e4a912
