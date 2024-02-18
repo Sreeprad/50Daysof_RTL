edbe58403e08c0028215eae613e86144ed473ac0
Sreeprad <mandasreeprad1104@gmail.com>
2024-02-18 10:27:36 +0530

    Updated Codes

--- 10. CARRY SELECT ADDER/CSLA_TB.v
+++ 10. CARRY SELECT ADDER/CSLA_TB.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module CSLA_TB;
 
     reg A0, A1, A2, A3, B0, B1, B2, B3, Cin = 0, Cin1 = 0, Cin2 = 1;
@@ -33,40 +32,4 @@ module CSLA_TB;
         $finish();
     end
 
-=======
-module CSLA_TB;
-
-    reg A0, A1, A2, A3, B0, B1, B2, B3, Cin = 0, Cin1 = 0, Cin2 = 1;
-    wire Cout, S0, S1, S2, S3;
-
-    CSLA_design uut(
-        .A0(A0),
-        .A1(A1),
-        .A2(A2),
-        .A3(A3),
-        .B0(B0),
-        .B1(B1),
-        .B2(B2),
-        .B3(B3),
-        .Cin(Cin),
-        .Cin1(Cin1),
-        .Cin2(Cin2),
-        .Cout(Cout),
-        .S0(S0),
-        .S1(S1),
-        .S2(S2),
-        .S3(S3)
-    );
-
-    initial begin
-        A0 = 0; B0 = 0; A1 = 1; B1 = 0; A2 = 0; B2 = 0; A3 = 1; B3 = 0;
-        #20
-        A0 = 1; B0 = 0; A1 = 1; B1 = 1; A2 = 0; B2 = 1; A3 = 1; B3 = 1;
-        #20
-        A0 = 1; B0 = 1; A1 = 1; B1 = 1; A2 = 1; B2 = 1; A3 = 1; B3 = 1;
-        #20
-        $finish();
-    end
-
->>>>>>> 3e3c4830248ac0eb2b91db77ea6c2fab36e4a912
 endmodule
--- 11. CARRY BYPASS ADDER/MUX_design.v
+++ 11. CARRY BYPASS ADDER/MUX_design.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module MUX_design (
     input D0,
     input D1,
@@ -10,17 +9,5 @@ module MUX_design (
     and U2(w2, D0, w1);
     and U3(w3, D1, S);
     or U4(Vout, w2, w3);
-=======
-module MUX_design (
-    input D0,
-    input D1,
-    input S,
-    output Vout
-    );
-    wire w1, w2, w3;
-    not U1(w1, S);
-    and U2(w2, D0, w1);
-    and U3(w3, D1, S);
-    or U4(Vout, w2, w3);
->>>>>>> 3e3c4830248ac0eb2b91db77ea6c2fab36e4a912
+
 endmodule
