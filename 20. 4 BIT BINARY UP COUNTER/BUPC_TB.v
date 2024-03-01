f338f7ca06c241d1987ec81522b1bfa06f6fd935
Sreeprad <mandasreeprad1104@gmail.com>
2024-03-02 01:11:43 +0530

    DAY 20 Files Added

---
+++ 20. 4 BIT BINARY UP COUNTER/BUPC_TB.v
@@ -0,0 +1,20 @@
+module BUPC_TB();
+
+  reg clk, reset;
+  wire [3:0] counter;
+
+  BUPC_design dut(clk, reset, counter);
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    reset = 1;
+    #20;
+    reset = 0;
+    #50;
+  end
+
+endmodule
